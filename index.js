b22fbf3a3f77936224f85fcb2a4075eef2e1bd56
Daniel R. Azulay <daniel@danielazulay.eu>
2022-06-08 06:39:32 +0200

    Cleanup

--- index.js
+++ index.js
@@ -1,42 +1,53 @@
 import config from './config.js'
 import Kraken from 'kraken-api'
-import * as timers from "timers";
+import { setInterval } from "timers";
 const kraken = new Kraken(config.key, config.secret);
 
-const resultToTicker = (res) => {
-	let r = res.result[config.order_params.pair]
-	return {
-		ticker: config.order_params.pair,
-		low: r.l,
-		high: r.h,
-		price: r.o
-	}
-}
-
-const resultToOrder = (res) => {
+/**
+ * Gets result from response object
+ * @param res
+ * @returns {RemoteObject | PropertyDescriptor[] | SearchMatch[] | Runtime.RemoteObject | ScriptCoverage[] | ScriptTypeProfile[] | T | SVGAnimatedString | string | ArrayBuffer}
+ */
+const getResult = (res) => {
 	return res.result
 }
 
-const getTicker = async () => {
+/**
+ * Displays the current ticker information
+ * @returns {Promise<void>}
+ */
+const getTickerData = async () => {
 	await kraken.api('Ticker', { pair: config.order_params.pair }, (err, res) => {
-		console.log(resultToTicker(res))
+		if (err !== null) {
+			console.error('An error has occurred', err)
+			return;
+		}
+		console.log(getResult(res))
 	});
 }
 
+/**
+ * Place an order
+ * @returns {Promise<void>}
+ */
 const placeOrder = async () => {
 	await kraken.api('AddOrder', config.order_params, (err, res) => {
 		if (err !== null) {
 			console.error('An error has occurred', err)
 			return;
 		}
-		console.log('Order placed', resultToOrder(res))
+		console.log('Order placed', getResult(res))
 	});
 }
 
+/**
+ * Main loop consisting of two intervals,
+ * one for orders and the other for ticker data
+ */
 (async () => {
 	await placeOrder()
-	timers.setInterval(placeOrder, config.interval_ms)
+	setInterval(placeOrder, config.order_interval_ms)
 	// Add order and wait interval
-	await getTicker()
-	timers.setInterval(getTicker, 10000)
+	await getTickerData()
+	setInterval(getTickerData, config.ticker_interval_ms)
 })();
